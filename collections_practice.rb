b7b51d8897a29061c3de49fb7144eec556b5c0e3
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-16 18:28:21 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,35 @@
+def sort_array_asc(array)
+  newArray = []
+  newArray = array.sort
+end 
 
+
+def sort_array_desc(array)
+  newArray = []
+  newArray = array.sort.reverse
+end 
+
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.length <=> b.length 
+  end 
+  
+end
+
+
+def swap_elements(array)
+  array[1],array[2] = array[2],array[1]
+  array
+end 
+
+def reverse_array(array)
+  array.reverse 
+end 
+
+def kesha_maker(array)
+    newArray = []
+    array.each do |val|
+      
+    end 
+end 
